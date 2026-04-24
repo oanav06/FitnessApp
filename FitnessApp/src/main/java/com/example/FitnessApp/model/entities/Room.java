96dbada079d1efced9ff2e47d92f40d9da9295bd
Oana <vrajitoruoana9@gmail.com>
2026-04-24 23:21:13 +0300

    implemented Hibernate Relations

--- FitnessApp/src/main/java/com/example/FitnessApp/model/entities/Room.java
+++ FitnessApp/src/main/java/com/example/FitnessApp/model/entities/Room.java
@@ -1,9 +1,15 @@
 package com.example.FitnessApp.model.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "rooms")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Room {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
